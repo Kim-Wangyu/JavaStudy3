b17f3b3cf73a8b3991e548668693a1013f6b9257
Kim-Wangyu <wang90205@naver.com>
2022-01-05 09:07:23 +0900

    studentutil

--- src/com/google/s1/student/StudentMain.java
+++ src/com/google/s1/student/StudentMain.java
@@ -3,11 +3,22 @@ package com.google.s1.student;
 public class StudentMain {
 
 	public static void main(String[] args) {
-		//makeStudent 호출
-		//이름,번호,국어 등이 출력
-		//리턴사용
-		
-		StudentMain sm =new StudentMain();
+		// makeStudent 호출
+		// 이름,번호,국어 등이 출력
+		// 리턴사용
+
+		StudentUtil su = new StudentUtil();
+		Student[] students = su.makeStudents();
+
+		for (int i = 0; i < students.length; i++) {
+			Student student = students[i];
+			System.out.println("이름은" + student.name);
+			System.out.println("번호는" + student.num);
+			System.out.println("국어는" + student.kor);
+			System.out.println("영어는" + student.eng);
+			System.out.println("수학은" + student.math);
+
+		}
 
 	}
 
--- src/com/google/s1/student/StudentUtil.java
+++ src/com/google/s1/student/StudentUtil.java
@@ -4,6 +4,38 @@ import java.util.Scanner;
 
 public class StudentUtil {
 
+	
+	public Student[] makeStudents() {
+		//학생의 수를 입력 받음
+		//키보드로부터 이름,번호,국어 영어수학입력
+		//학생들의 정보를 리턴
+		Scanner sc= new Scanner(System.in);
+		Student stu = new Student();
+		
+		
+		System.out.println("학생수 입력");
+		int count=sc.nextInt();
+		Student [] students = new Student[count];
+		for(int i=0;i<students.length;i++) {
+		Student student = new Student();
+		
+			
+			System.out.println("학생이름 입력");
+		student.name=sc.next();
+		System.out.println("번호 입력");
+		student.num=sc.nextInt();
+		System.out.println("국어 입력");
+		student.kor=sc.nextInt();
+		System.out.println("영어 입력");
+		student.eng=sc.nextInt();
+		System.out.println("수학 입력");
+		student.math=sc.nextInt();
+		students[i]=student;
+		
+		
+		}
+		return students;
+	}
 	public Student makeStudent() {
 		// 키보드로부터 이름, 번호, 국어,영어,수학을 입력받아
 		// 입력받은것들을 리턴
