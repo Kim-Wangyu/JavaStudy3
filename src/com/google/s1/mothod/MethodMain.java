71a4722dc0794044a79942478a3472aae0e1a788
Kim-Wangyu <97069537+Kim-Wangyu@users.noreply.github.com>
2022-01-05 09:43:30 +0900

    Update Github

--- src/com/google/s1/mothod/MethodMain.java
+++ src/com/google/s1/mothod/MethodMain.java
@@ -5,6 +5,7 @@ public class MethodMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Start");
+System.out.println("깃허브에서작성");		
 System.out.println("FINISH");
 	}
 
